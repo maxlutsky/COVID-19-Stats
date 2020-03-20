77e1d2a0d60ed59697cd8a290c5b0bed7f52c459
Max <gmax115230@gmail.com>
2020-03-20 10:32:45 +0200

    first commit

---
+++ COVID-19 Stats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-19 Stats
+//
+//  Created by Max on 20/03/2020.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
