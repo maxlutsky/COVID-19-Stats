8fc414ab5568da6551446e8cc43ccc502197dc1e
Max <gmax115230@gmail.com>
2020-03-29 13:27:44 +0300

    added Value of the switch button to User Defaults. Created button to remove all data from app.

--- COVID-19 Stats/SettingsViewController.swift
+++ COVID-19 Stats/SettingsViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+//    let dataService = DataService.shared
+    
     @IBOutlet weak var testSwitchOutlet: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        testSwitchOutlet.isOn = UserDefaults.standard.bool(forKey: "testSwitch")
         // Do any additional setup after loading the view.
     }
     
@@ -21,8 +23,10 @@ class SettingsViewController: UIViewController {
     @IBAction func testSwitch(_ sender: Any) {
         if testSwitchOutlet.isOn{
             print("Switch was turned on")
+            UserDefaults.standard.set(true, forKey: "testSwitch")
         } else {
             print("Switch was turned off")
+            UserDefaults.standard.set(false, forKey: "testSwitch")
         }
     }
     
@@ -36,4 +40,17 @@ class SettingsViewController: UIViewController {
     }
     */
 
+    @IBAction func removeDataButtonAction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Are you sure that you want to remove all data?", message: "You will not be able to undo this. This action will remove all stored information.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+//            self.dataService.fetchAndDelete()
+        }))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .default, handler: { _ in
+            NSLog("The \"Cancel\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
